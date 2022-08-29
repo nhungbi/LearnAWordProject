1e9dd27411350ff5b48e98a358f6cff3c026fd8e
nhungbi <nhungvan16@gmail.com>
2022-08-29 14:22:23 -0500

    hide secret_key

--- backend/word_project/settings.py
+++ backend/word_project/settings.py
@@ -18,9 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)wl#p&2&n%94p%t92ib!dh*i7bp%p40mig1lp-=aw3+$gj5iz_"
+SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
